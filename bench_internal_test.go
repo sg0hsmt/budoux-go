33a624960420a507492ae61564f86a4249436331
Shigeo Hashimoto <sg0hsmt@gmail.com>
2022-05-05 20:04:31 +0900

    Add internal benchmark

--- bench_internal_test.go
+++ bench_internal_test.go
@@ -1,6 +1,7 @@
 package budoux
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -10,5 +11,17 @@ func BenchmarkGetUnicodeBlockAndFeature(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		getUnicodeBlockAndFeature(in, 27)
+	}
+}
+
+func BenchmarkGetScore(b *testing.B) {
+	model := Model{}
+	for i := 0; i < 300; i++ {
+		model[fmt.Sprintf("TEST:%03d", i)] = i
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		getScore(model, "日", "本", "語", "の", "文", "章", "120", "120", "120", "108", "120", "120", "O", "O", "O")
 	}
 }
